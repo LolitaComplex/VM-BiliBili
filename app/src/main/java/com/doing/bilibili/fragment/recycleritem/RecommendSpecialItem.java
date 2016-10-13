ebb3212896335bc80762b5d1521ca31d7847a320
LolitaComplex <1024587616@qq.com>
2016-10-13 22:20:26 +0800

    全部功能都做果然吃不消，完全就是体力活。之后只制作对自己有用的模块。(预定：视频、直播、搜索、设置、主题、评论回复、下载)

---
+++ app/src/main/java/com/doing/bilibili/fragment/recycleritem/RecommendSpecialItem.java
@@ -0,0 +1,39 @@
+package com.doing.bilibili.fragment.recycleritem;
+
+import android.content.Context;
+import android.text.TextUtils;
+
+import com.doing.bilibili.R;
+import com.doing.bilibili.baselib.adapter.recyclerview.BaseViewHolder;
+import com.doing.bilibili.entity.recommend.Recommend;
+
+/**
+ * Created by Doing on 2016/10/13 0013.
+ *
+ */
+public class RecommendSpecialItem extends ItemViewDelegateImp<Recommend> {
+
+    public RecommendSpecialItem(Context context) {
+        super(context);
+    }
+
+    @Override
+    public int getItemViewLayoutId() {
+        return R.layout.item_recommand_special;
+    }
+
+    @Override
+    public boolean isForViewType(Recommend item, int position) {
+        return TextUtils.isEmpty(item.getType());
+    }
+
+    @Override
+    public void convert(BaseViewHolder holder, Recommend recommend, int position) {
+        holder.setImageUrl(R.id.RecommendSpecialItem_iv, recommend.getBody().get(0).getCover());
+    }
+
+    @Override
+    public boolean isEnable() {
+        return false;
+    }
+}
